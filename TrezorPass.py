d4d0bf9d17c3ebbf8753f673495d50a4934f4652
Ondrej Mikle <ondrej.mikle@nic.cz>
2014-08-15 10:50:27 +0200

    Choosing Trezor device

--- TrezorPass.py
+++ TrezorPass.py
@@ -2,6 +2,8 @@
 import sys
 
 from PyQt4.QtGui import QApplication, QMainWindow
+#from PyQt4.QtCore import QSettings
+
 from ui_mainwindow import Ui_MainWindow
 
 from trezorlib.client import TrezorClient
@@ -17,37 +19,69 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 class Trezor(object):
 	"""Class for working with Trezor device via HID"""
 	
-	def getDevice(self):
-		"""Get one from available devices"""
+	def __init__(self):
+		pass
+	
+	def getDevice(self, callback):
+		"""
+		Get one from available devices. Callback chooses index
+		of device, see chooseDevice callback.
+		"""
 		devices = self.enumerateHIDDevices()
 
 		if not devices:
 			return None
 		
-		transport = self.chooseDevice(devices)
+		transport = self.chooseDevice(devices, callback)
 		client = TrezorClient(transport)
 
 		return client
 
 	def enumerateHIDDevices(self):
+		"""Returns Trezor HID devices"""
 		devices = HidTransport.enumerate()
 
 		return devices
 
-	def chooseDevice(self, devices):
+	def chooseDevice(self, devices, callback):
+		"""
+		Choose device from enumerated list. Callback gets list of
+		tuples (index, string) which denote labels of connected trezors.
+		The callback returns index of device that should be used.
+		"""
 		if not len(devices):
-			raise Exception("No Trezor connected!")
+			raise RuntimeError("No Trezor connected!")
 
-		# always tries the one it finds
-		try:
-			return HidTransport(devices[0])
-		except IOError:
-			raise Exception("Device is currently in use")
+		if len(devices) == 1:
+			try:
+				return HidTransport(devices[0])
+			except IOError:
+				raise RuntimeError("Trezor is currently in use")
+		
+		deviceTuples = []
+		
+		for idx, device in enumerate(devices):
+			try:
+				transport = HidTransport(devices[0])
+				client = TrezorClient(transport)
+				label = client.features.label and client.features.label or "<no label>"
+				deviceTuples += [(idx, label)]
+			except IOError:
+				#device in use, do not offer as choice
+				continue
+				
+		if not deviceTuples:
+			raise RuntimeError("All connected Trezors are in use!")
+		
+		chosenDevice = callback(deviceTuples)
+		return deviceTuples[chosenDevice][1]
+		
 
 
 trezor = Trezor()
-client = trezor.getDevice()
-print client
+trezorChooseCallback = lambda deviceTuples: 0
+client = trezor.getDevice(trezorChooseCallback)
+print "label:", client.features.label
 
 app = QApplication(sys.argv)
 mainWindow = MainWindow()
